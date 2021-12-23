ad5adabdd97a800bada50b11eac275f9eeaff4ce
Farah A <farahwamer@gmail.com>
2021-12-23 17:43:21 -0500

    created link components

--- src/components/home/Link.js
+++ src/components/home/Link.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Link.css";
+import "./Link.css";
 
-const Link = () => {
-  return <div></div>;
+const Link = (props) => {
+  return (
+    <div className="link-container">
+      <p className="link-url">{props.url}</p>
+      <a href={props.link} target="_blank" className="link-link">
+        {props.title}
+      </a>
+      <p className="link-text">{props.text}</p>
+    </div>
+  );
 };
 
 export default Link;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,51 @@
 import React from "react";
 import "./Home.css";
-import Dictionary from "/Users/farahamer/Desktop/portfolio/src/components/home/Dictionary.js";
-import Accordion from "/Users/farahamer/Desktop/portfolio/src/components/home/Accordion.js";
+import Dictionary from "../../components/home/Dictionary.js";
+import Accordion from "../../components/home//Accordion.js";
+import Link from "../../components/home/Link.js";
 
 const Home = () => {
+  const linkInfo = [
+    {
+      url: "file:///Users/farahamer/Documents/resume.pdf",
+      title: "Resume",
+      text: "Check out Farah’s qualifications and work experience!",
+      link: "/assets/Farah-Amer-Resume.pdf",
+    },
+    {
+      url: "https://www.linkedin.com > farah-amer",
+      title: "LinkedIn | Farah Amer - Queen's University ...",
+      text: "View Farah Amer's profile on LinkedIn, the world's largest professional community. Farah has 4 jobs listed on their profile. See the complete profile on LinkedIn ...",
+      link: "https://www.linkedin.com/in/farah-amer/",
+    },
+    {
+      url: "https://github.com > farahwamer",
+      title: "Github | @farahwamer",
+      text: "farahwamer has 3 repositories available. Follow their code on Github.",
+      link: "https://github.com/farahwamer",
+    },
+    {
+      url: "https://dribbble.com > wamerz",
+      title: "Dribbble | @wamerz",
+      text: "Explore this designers work on Dribbble, the best place for to designers gain inspiration, feedback, community, and jobs worldwide.",
+      link: "https://dribbble.com/wamerz",
+    },
+  ];
+
   return (
     <div className="home-container">
       <Dictionary />
       <Accordion />
+      {linkInfo.map((obj) => {
+        return (
+          <Link
+            url={obj.url}
+            title={obj.title}
+            text={obj.text}
+            link={obj.link}
+          />
+        );
+      })}
     </div>
   );
 };
